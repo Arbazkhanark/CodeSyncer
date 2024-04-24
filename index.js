05ce4f4e01337eb092ed7ef8a5c18b704072e446
Arbaaz khan <arbaazkhan23@gmail.com>
2024-04-24 12:57:59 +0530

    try1

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 // app.get("/",(req,res)=>res.send("Namaste Code"))
-app.use("api",userRouter);
+app.use("/api",userRouter);
 
 app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname,"dist","index.html"))
